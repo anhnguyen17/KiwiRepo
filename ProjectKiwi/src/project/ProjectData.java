20f0c5fdd99ff37127a8b6cbc0eba77be2f926e6
Thomas Ayele <thomasayele17@augustana.edu>
2018-09-20 16:54:57 -0500

    Fixed commit issue on my Video.java class

--- ProjectKiwi/src/project/ProjectData.java
+++ ProjectKiwi/src/project/ProjectData.java
@@ -7,5 +7,22 @@ public class ProjectData {
 	private List<AnimalTrack> tracks;
 	private Video video;
 	
+	public ProjectData() {
+		
+	}
+	
+	public List<AnimalTrack> getTracks() {
+		return tracks;
+	}
+	public void setTracks(List<AnimalTrack> tracks) {
+		this.tracks = tracks;
+	}
+	public Video getVideo() {
+		return video;
+	}
+	public void setVideo(Video video) {
+		this.video = video;
+	}
+	
 
 }
