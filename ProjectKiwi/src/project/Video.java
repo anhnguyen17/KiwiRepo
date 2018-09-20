c8d4b25e5814d2c60d9e9c43ddb1a861f990d90d
anhnguyen17 <anhnguyen17@augustana.edu>
2018-09-20 16:45:33 -0500

    Modify Video Class with new constructor

--- ProjectKiwi/src/project/Video.java
+++ ProjectKiwi/src/project/Video.java
@@ -12,69 +12,88 @@ public class Video {
 	private double xPixelsPerCm;
 	private double yPixelsPerCM;
 	private int totalNumFrames;
-	private String filePath;
+	private String fileName;
 	private int startFrameNum;
 	private int endFrameNum;
 	private Rectangle arenaBounds;
 	
-	public Video(double FrameRate) {
+	public Video(double frameRate, String fileName, int totalNumFrames) {
+		super();
 		this.frameRate = frameRate;
+		this.fileName = fileName;
+		this.totalNumFrames = totalNumFrames;
 	}
+	
+
 	public double getFrameRate() {
 		return frameRate;
 	}
+
 
 	public void setFrameRate(double frameRate) {
 		this.frameRate = frameRate;
 	}
 
+
 	public double getxPixelsPerCm() {
 		return xPixelsPerCm;
 	}
+
 
 	public void setxPixelsPerCm(double xPixelsPerCm) {
 		this.xPixelsPerCm = xPixelsPerCm;
 	}
 
+
 	public double getyPixelsPerCM() {
 		return yPixelsPerCM;
 	}
+
 
 	public void setyPixelsPerCM(double yPixelsPerCM) {
 		this.yPixelsPerCM = yPixelsPerCM;
 	}
 
+
 	public int getTotalNumFrames() {
 		return totalNumFrames;
 	}
+
 
 	public void setTotalNumFrames(int totalNumFrames) {
 		this.totalNumFrames = totalNumFrames;
 	}
 
-	public String getFilePath() {
-		return filePath;
+
+	public String getFileName() {
+		return fileName;
 	}
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
+
 
 	public int getStartFrameNum() {
 		return startFrameNum;
 	}
 
+
 	public void setStartFrameNum(int startFrameNum) {
 		this.startFrameNum = startFrameNum;
 	}
+
 
 	public int getEndFrameNum() {
 		return endFrameNum;
 	}
 
+
 	public void setEndFrameNum(int endFrameNum) {
 		this.endFrameNum = endFrameNum;
 	}
+
 
 	public Rectangle getArenaBounds() {
 		return arenaBounds;
