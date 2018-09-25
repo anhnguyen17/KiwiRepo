89d5fcc3a5e044ded168d3be55b57494f21ea936
Genesis Sarmiento <genesissarmiento17@augustana.edu>
2018-09-24 20:06:25 -0500

    modified Video class

--- ProjectKiwi/src/project/Video.java
+++ ProjectKiwi/src/project/Video.java
@@ -1,5 +1,10 @@
 package project;
+
 import java.awt.*;
+import java.io.FileNotFoundException;
+
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
 
 /**
  * @author anhnguyen17
@@ -7,69 +12,67 @@ import java.awt.*;
  */
 
 public class Video {
-	private double xPixelsPerCm;
-	private double yPixelsPerCm;
-	private String fileName;
+	
+	private String filePath;
+	private VideoCapture vidCap;
 	private int startFrameNum;
 	private int endFrameNum;
+	
+	private double xPixelsPerCm;
+	private double yPixelsPerCm;
 	private Rectangle arenaBounds; 
-	private double frameNum;
 	
-	public Video(String fileName) {
-		super();
-		this.fileName = fileName;
-		
+	
+	public Video(String filePath) throws FileNotFoundException {
+		this.filePath = filePath;
+		this.vidCap = new VideoCapture(filePath);
+		if (!vidCap.isOpened()) {
+			throw new FileNotFoundException("Unable to open video file: " + filePath);
+		}		
 	}
 	
-
-	public double getxPixelsPerCm() {
-		return xPixelsPerCm;
+	public String getFilePath() {
+		return this.filePath;
+	}
+	public double getFrameRate() {
+		return vidCap.get(Videoio.CAP_PROP_FPS);
+	}
+	public int getTotalNumFrames() {
+		return (int) vidCap.get(Videoio.CAP_PROP_FRAME_COUNT);
 	}
 
-
-	public void setxPixelsPerCm(double xPixelsPerCm) {
-		this.xPixelsPerCm = xPixelsPerCm;
-	}
-
-
-	public double getyPixelsPerCm() {
-		return yPixelsPerCm;
-	}
-
-
-	public void setyPixelsPerCm(double yPixelsPerCm) {
-		this.yPixelsPerCm = yPixelsPerCm;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-
+	
 	public int getStartFrameNum() {
 		return startFrameNum;
 	}
-
-
+	
 	public void setStartFrameNum(int startFrameNum) {
 		this.startFrameNum = startFrameNum;
 	}
-
 
 	public int getEndFrameNum() {
 		return endFrameNum;
 	}
 
-
 	public void setEndFrameNum(int endFrameNum) {
 		this.endFrameNum = endFrameNum;
 	}
 
+	public double getXPixelsPerCm() {
+		return xPixelsPerCm;
+	}
+
+	public void setXPixelsPerCm(double xPixelsPerCm) {
+		this.xPixelsPerCm = xPixelsPerCm;
+	}
+
+	public double getYPixelsPerCm() {
+		return yPixelsPerCm;
+	}
+
+	public void setYPixelsPerCm(double yPixelsPerCm) {
+		this.yPixelsPerCm = yPixelsPerCm;
+	}
 
 	public Rectangle getArenaBounds() {
 		return arenaBounds;
